d83f2532b33d394cc297a756b77ce767c0ddf948
Alejandro Martinez <103444752+AMDevelopOB@users.noreply.github.com>
2022-11-06 23:08:56 +0100

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,7 @@
 import  * as mongoDB from 'mongodb';
 
 const MONGODB_CONNECTION =
-  "mongodb+srv://forumMaster:O9lUjoHg46pvjXRr@graphorum.4bovnxr.mongodb.net/?retryWrites=true&w=majority";
+  "";
 
 const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGODB_CONNECTION);
            
